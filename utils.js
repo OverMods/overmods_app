64b66dc4c88a0311688cc94e36bd885dad7d3d1c
nikolaytihonov <nikolay_tihonov@i.ua>
2023-06-21 16:00:35 +0300

    Use strptime library instead of strftime

--- utils.js
+++ utils.js
@@ -1,9 +1,14 @@
-import strftime from "strftime";
+import {strftime, strptime} from "strtime";
+export const TIME_FORMAT = "%y-%m-%d %H:%M:%S";
 
 export function formatSqlTime(date) {
-    return strftime("%y-%m-%d %H:%M:%S", date);
+    return strftime(date, TIME_FORMAT);
 }
 
 export function sqlTimeNow() {
     return formatSqlTime(new Date());
+}
+
+export function convertToDate(string) {
+    return strptime(string, TIME_FORMAT)
 }
