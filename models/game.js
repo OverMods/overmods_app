8dc55268cd8ccbece88670d4f52f453af38a13a6
nikolaytihonov <nikolay_tihonov@i.ua>
2023-06-24 15:29:14 +0300

    Implement game model

---
+++ models/game.js
@@ -0,0 +1,68 @@
+import { Model } from "./model.js";
+import knex from "../db.js";
+
+export class Game extends Model {
+    constructor(id) {
+        super(id);
+        this.title = null;
+        this.logo = null;
+    }
+
+    async fromJson(json) {
+        this.title = json.title;
+        if (this.logo) {
+            this.logo = json.logo;
+        }
+    }
+    async toJson() {
+        return {
+            title: this.title,
+            logo: this.logo
+        }
+    }
+
+    async fromDataBase(data) {
+        this.title = data.title;
+        this.logo = data.logo || null;
+    }
+
+    async create() {
+        await knex("game").insert({
+            title: this.title,
+            logo: this.logo
+        });
+    }
+
+    async read() {
+        const data = await knex("game")
+            .select("*")
+            .where("id","=",this.getId())
+            .limit(1);
+        if (data.length > 0) {
+            await this.fromDataBase(data[0]);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    async update() {
+        const data = {};
+        if (this.title) {
+            data.title = this.title;
+        }
+        if (this.logo) {
+            data.logo = this.logo;
+        }
+
+        await knex("game")
+            .update(data)
+            .where("id","=",this.getId());
+    }
+
+    async delete() {
+        await knex("game")
+            .where("id","=",this.getId())
+            .delete();
+    }
+}
