9ded7444fdce03db5bf783a521a1764528b11aef
mykola2312 <nikolay_tihonov@i.ua>
2023-07-05 13:02:22 +0300

    Implement request role model. Add approve/decline logic

--- models/request.js
+++ models/request.js
@@ -37,6 +37,23 @@ export class Request extends Model {
         this.status = null;
     }
 
+    async consider(by, status) {
+        this.consideredBy = by;
+        this.consideredAt = new Date();
+        this.status = status;
+        await this.update({});
+    }
+
+    async approve(by) {
+        await this.consider(by, Status.APPROVED);
+        return true;
+    }
+
+    async decline(by) {
+        await this.consider(by, Status.DECLINED);
+        return true;
+    }
+
     sanitizeCheck(json) {
         return true;
     }
@@ -78,5 +95,31 @@ export class Request extends Model {
         };
         // {...obj1, ...obj2} is used to merge two objects, where obj2 overrides obj1
         await knex(this.table).insert({...base, ...child});
+    }
+
+    async update(child) {
+        const base = {};
+        if (this.requestedBy) {
+            base.requested_by = this.requestedBy;
+        }
+        if (this.requestedAt) {
+            base.requested_at = formatSqlTime(this.requestedAt);
+        }
+        if (this.requestText) {
+            base.request_text = this.requestText;
+        }
+        if (this.consideredBy) {
+            base.considered_by = this.consideredBy;
+        }
+        if (this.consideredAt) {
+            base.considered_at = formatSqlTime(this.consideredAt);
+        }
+        if (this.status) {
+            base.status = this.status.getStatusName();
+        }
+
+        await knex(this.table)
+            .update({...base, ...child})
+            .where("id","=",this.getId());
     }
 }
---
+++ models/requestRole.js
@@ -0,0 +1,47 @@
+import { Request } from "./request.js";
+import { User, Role } from "./user.js";
+import { errors } from "../error.js";
+
+export class RequestRole extends Request {
+    constructor(id) {
+        super(id, "request_role");
+        this.newRole = null;
+    }
+
+    async approve(by) {
+        const user = new User(this.requestedBy);
+        if (!await user.read()) {
+            return errors.USER_NOT_FOUND;
+        }
+
+        user.role = this.newRole;
+        await user.update();
+
+        return await super.approve(by);
+    }
+
+    async fromJson(json) {
+        await super.fromJson(json);
+        this.newRole = new Role(json.newRole);
+    }
+
+    async toJson() {
+        const base = await super.toJson();
+        const json = {
+            newRole: this.newRole.getRoleName()
+        };
+        return {...base, ...json};
+    }
+
+    async create() {
+        await super.create({
+            new_role: this.newRole.getRoleName()
+        });
+    }
+
+    async update() {
+        await super.update({
+            new_role: this.newRole.getRoleName()
+        });
+    }
+}
