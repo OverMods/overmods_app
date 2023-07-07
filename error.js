aadfa99db5459c288c7f5b11d8b095cee60942fb
mykola2312 <nikolay_tihonov@i.ua>
2023-07-07 21:18:26 +0300

    Update error class and error function to achieve more flexible error-responding

--- error.js
+++ error.js
@@ -12,6 +12,13 @@ class APIError {
         return this._text;
     }
 
+    toJson() {
+        return {
+            code: this.code,
+            text: this.text
+        };
+    }
+
     toString() {
         return `API Error ${this.code}: ${this.text}`;
     }
@@ -37,24 +44,36 @@ export class APIException extends Error {
     }
 }
 
+export class BanError extends APIError {
+    constructor(ban) {
+        super(11, "Banned");
+        this.reason = ban ? ban.reason : null;
+    }
+
+    toJson() {
+        const base = super.toJson();
+        const json = {reason: this.reason};
+        return {...base, ...json};
+    }
+}
+
 export const errors = {
-    INVALID_PARAMETER: new APIError(1, "Invalid parameter"),
-    UNAUTHORIZED: new APIError(2, "Unauthorized"),
-    INSUFFICIENT_PRIVILEGES: new APIError(3, "Insufficient privileges"),
-    USER_NOT_FOUND: new APIError(4, "User not found"),
-    INVALID_PASSWORD: new APIError(5, "Invalid password"),
-    ALREADY_AUTHORIZED: new APIError(6, "Already logged in"),
-    NOT_FOUND: new APIError(7, "Not found"),
-    FAILED: new APIError(8, "Failed"),
-    USER_ALREADY_EXISTS: new APIError(9, "User already exists"),
-    NOT_MODIFIED: new APIError(10, "Not modified")
+    INVALID_PARAMETER: () => new APIError(1, "Invalid parameter"),
+    UNAUTHORIZED: () => new APIError(2, "Unauthorized"),
+    INSUFFICIENT_PRIVILEGES: () => new APIError(3, "Insufficient privileges"),
+    USER_NOT_FOUND: () => new APIError(4, "User not found"),
+    INVALID_PASSWORD: () => new APIError(5, "Invalid password"),
+    ALREADY_AUTHORIZED: () => new APIError(6, "Already logged in"),
+    NOT_FOUND: () => new APIError(7, "Not found"),
+    FAILED: () => new APIError(8, "Failed"),
+    USER_ALREADY_EXISTS: () => new APIError(9, "User already exists"),
+    NOT_MODIFIED: () => new APIError(10, "Not modified"),
+    BANNED: (ban) => new BanError(ban)
 };
 
-export function error(res, error) {
+export function error(res, func) {
+    const error = func(...Object.values(arguments).slice(2));
     res.json({
-        error: {
-            code: error.code,
-            text: error.text
-        }
+        error: error.toJson()
     });
 }
